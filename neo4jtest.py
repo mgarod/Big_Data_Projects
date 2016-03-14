9adf705c51904e828efcec68789ce5a99785b453
Michael Garod <mgarod@gmail.com>
2016-03-14 19:48:45 -0400

    Add neo4jtest.py to confirm neo4j functions

---
+++ neo4jtest.py
@@ -0,0 +1,19 @@
+from py2neo import Graph, Node, Relationship, authenticate
+
+graph = Graph()  # Makes connection to http://localhost:7474
+graph.delete_all()  # Clear the graph
+
+Michael = Node("Person", name="Michael", job="Student", age=26)
+Felice = Node("Person", name="Felice", job="Web Developer", age=32)
+Jackie = Node("Person", name="Jackie", job="Graphic Designer", age=31)
+Anny = Node("Person", name="Anny", job="Consultant", age=32)
+
+Michael_dates_Felice = Relationship(Michael, "Dates", Felice, Since=2015)
+Felice_knows_Jackie = Relationship(Felice, "Knows", Jackie, Since=1990)
+Anny_knows_Michael = Relationship(Anny, "Knows", Michael, Since=2016)
+
+graph.create(Felice_knows_Jackie)
+graph.create(Michael_dates_Felice)
+graph.create(Anny_knows_Michael)
+
+# Visit http://localhost:7474 to view the GUI and practice CypherQL
