ed0dc16e5296e133b26495cfbdddbc44d83e8ea9
ryankall <ryan11432@gmail.com>
2016-03-24 16:04:34 -0400

    Update Query2.py

--- Prog/Query2.py
+++ Prog/Query2.py
@@ -5,7 +5,7 @@ def Trusted_colleague(Fname, Lname):
     graph = Graph()  # Makes connection to http://127.0.0.1:7474
     cypher = graph.cypher
     result = cypher.execute(
-        "MATCH (p:Person{Fname: {first_name}, Lname: {last_name}})-[r:colleague*2]->(Similar_p)  "
+        "MATCH (p:Person{Fname: upper({first_name}), Lname: upper({last_name})})-[r:colleague*2]->(Similar_p)  "
         "MATCH (Similar_p)-[ri:interested_in]->(s)  "
         "MATCH (p)-[:interested_in]->(common_interest)<-[:interested_in]-(Similar_p)   "
         "return DISTINCT Similar_p.Fname, Similar_p.Lname", first_name = Fname, last_name = Lname)
