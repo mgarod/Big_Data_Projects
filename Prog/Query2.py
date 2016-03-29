ef2528e4297871803f20d045141325d334610177
ryankall <ryan11432@gmail.com>
2016-03-29 15:51:06 -0400

    Update Query2.py

--- Prog/Query2.py
+++ Prog/Query2.py
@@ -1,89 +1,44 @@
 from py2neo import Graph
+import Validators as valid
 
 graph = Graph()  # Makes connection to http://127.0.0.1:7474
 
 
 def query2():
-    # Get nameF, nameL within this function
+    #get user id
+    u_id = valid.validate_num("$ Enter a user_id: ")
 
-    list = set()
-    namelist = set()
-
+    if not PersonExist(u_id):
+        print "User ID {} not found in database".format(u_id)
+        return
 
     #get the resource data from the query
-    result = graph.cypher.execute(
-                    "Match (person:Person {Fname: upper({name1}), Lname: upper({name2})})-[r:Work_at]->(job) "
-                    "Match (job)-[r1:DISTANCE]-(closeCompany) where r1.closeby = true "
-                    "Match (closePerson)-[:Work_at]->(closeCompany) "
-                    "Match (closePerson)-[ri:interested_in]->(cpInterest)<-[:interested_in]-(person) "
-                    "return closePerson.Fname AS PersonF, closePerson.Lname AS PersonL"
-                    ", closePerson.User_id AS Personid, TOINT(cpInterest.level) AS level,closeCompany.name AS company  , "
-                    "cpInterest.interestName order BY TOINT( cpInterest.level) DESC",
-                    name1 = nameF, name2 = nameL)
-
-
-    #return false if person is not found in database
-    if not PersonExist(nameF, nameL):
-        print 'This person does not exist in Database.'
-        return
+    result = graph.cypher.execute("""
+        MATCH (p:Person{User_id: {Uid} })-[r:colleague*2]-(Similar_p)
+        WHERE NOT  (p)-[:colleague]-(Similar_p)
+        MATCH (p)-[:interested_in]->(common_interest)<-[:interested_in]-(Similar_p)
+        return DISTINCT  Similar_p.Fname, Similar_p.Lname, p.Fname, p.Lname
+        """, Uid = u_id )
 
     if result.__len__() == 0:
-        print 'No results found for'+ nameF+ " "+ nameL
+        print 'No results found for User id:' + u_id
         return
 
+    print 'Trusted colleague of colleague that share a common interest with', result[0][2], result[0][3]
 
-    #put inorder
-    sortedarray = getRankOrder(result)
-    sortedarray.sort(None, key = lambda x: x[1], reverse = False)
-    printData(sortedarray, list, namelist, result)
-
-
-def getRankOrder(result):
-    #PreCondition: must be populated result table
-    table = {}
+    k = 1
     for index in result:
-        if table.has_key(index.Personid):
-            table[index.Personid] = table[index.Personid] + index.level
-        else:
-            table[index.Personid] = index.level
+        print "{} | {} {}".format(k,index[0], index[1])
+        k+=1
 
-    return table.items()
-
-
-
-def printData(sortedarray, list, namelist, result):
-    #read data out
-    while sortedarray.__len__() != 0:
-       current = sortedarray.pop()
-
-       #print common interest for a certain person
-       for x in result:
-
-            if list.__contains__(x[5]):
-               continue
-
-           #find p in x# namelist = set() # reset the interest set
-            if (current[0] == x[2]) :
-
-                if(not namelist.__contains__(current[0])):
-                    fullname ="Name: "
-                    fullname =fullname + x[0] + " " + x[1] + ", Working at:"+ x[4] +" id: "
-                    print fullname, current[0]
-
-                print "Common Interest: ", x[5], ", At level", x[3]
-                namelist.add(current[0])
-                list.add(x[5])
-
-       list = set()
-       namelist = set() # reset the interest set
-       print '\n'
-
-
-def PersonExist(Fname, Lname):
-
+def PersonExist(U_id):
+    """
+    Check id is in database
+    :param U_id: int
+    :return: true if found other false
+    """
     result = graph.cypher.execute(
-        "MATCH (p:Person{Fname: upper({first_name}), Lname: upper({last_name}) } ) return p",
-        first_name = Fname, last_name = Lname )
+        "MATCH (p:Person{User_id: {uid} } ) return p", uid = U_id)
 
     if result.__len__() > 0:
         return True
@@ -92,5 +47,4 @@ def PersonExist(Fname, Lname):
 
 
 
-
-
+query2()
