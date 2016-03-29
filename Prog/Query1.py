04b9f844ec301a783079206d32645b29e3eb540b
ryankall <ryan11432@gmail.com>
2016-03-29 15:50:05 -0400

    Update Query1.py

--- Prog/Query1.py
+++ Prog/Query1.py
@@ -18,33 +18,41 @@ def query1():
 
     #get the resource data from the query
     result = cypher.execute(
-                    "Match (person:Person {User_id: {uid}})-[r:Work_at]->(job) "
-                    "Match (job)-[r1:DISTANCE]-(closeCompany) where r1.closeby = true "
-                    "Match (closePerson)-[:Work_at]->(closeCompany) "
-                    "Match (closePerson)-[ri:interested_in]->(cpInterest)<-[:interested_in]-(person) "
-                    "return closePerson.Fname AS PersonF, closePerson.Lname AS PersonL"
-                    ", closePerson.User_id AS Personid, TOINT(cpInterest.level) AS level,closeCompany.name AS company  , "
-                    "cpInterest.interestName order BY TOINT( cpInterest.level) DESC",
-                    uid=u_id)
-
+        """
+        Match (person:Person {User_id: {uid}})-[r:Work_at]->(job)
+        Match (job)-[r1:DISTANCE]-(closeCompany) where r1.closeby = true
+        Match (closePerson)-[:Work_at]->(closeCompany)
+        Match (closePerson)-[ri:interested_in]->(cpInterest)<-[ri2:interested_in]-(person)
+        return closePerson.Fname AS PersonF, closePerson.Lname AS PersonL
+        , closePerson.User_id AS Personid, TOINT(ri2.level) AS level,closeCompany.name AS company,ri.level AS OtherLevel ,
+        cpInterest.interestName order BY TOINT( ri2.level) DESC
+        UNION
+        Match (person:Person {User_id: {uid}})-[r:Work_at]->(closeCompany)
+        Match (closePerson)-[:Work_at]->(closeCompany)
+        Match (closePerson)-[ri:interested_in]->(cpInterest)<-[ri2:interested_in]-(person)
+        return closePerson.Fname AS PersonF, closePerson.Lname AS PersonL
+        , closePerson.User_id AS Personid, TOINT(ri2.level) AS level,closeCompany.name AS company,ri.level AS OtherLevel,
+        cpInterest.interestName order BY TOINT( ri2.level) DESC
+        """,uid=u_id)
     sys.stdout.flush()
 
     #get ranking order
     table = {}
     for index in result:
         if table.has_key(index.Personid):
-            table[index.Personid] = table[index.Personid] + index.level
+            table[index.Personid] = table[index.Personid] + min(index.level, index.OtherLevel)
         else:
-            table[index.Personid] = index.level
+            table[index.Personid] = min(index.level, index.OtherLevel)
 
     #put inorder
     sortedarray = table.items()
     sortedarray.sort(None, key = lambda x: x[1], reverse = False)
-
+    print '\n'
+    
     #read data out
     while sortedarray.__len__() != 0:
+    
         current = sortedarray.pop()
-
         #print common interest for a certain person
         for x in result:
 
@@ -55,17 +63,20 @@ def query1():
             if (current[0] == x[2]) :
 
                 if(not namelist.__contains__(current[0])):
+
                     fullname ="Name: "
-                    fullname =fullname + x[0] + " " + x[1] + ", Working at:"+ x[4] +" id: "
+                    fullname =fullname + x[0] + " " + x[1] + ", Working at: "+ x[4] +"  User id: "
                     print fullname, current[0]
 
-                print "Common Interest: ", x[5], ", At level", x[3]
+                print "Common Interest: {} ({}) ".format( x[6], x[3])
+
                 namelist.add(current[0])
                 mylist.add(x[5])
 
         mylist = set()
         namelist = set() # reset the interest set
         print '\n'
+
 
 
 
