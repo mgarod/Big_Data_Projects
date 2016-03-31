d92277486533e979967a9a8bc0548598f5831cd6
ryankall <ryan11432@gmail.com>
2016-03-30 23:37:47 -0400

    Update Neo4jLoader.py

--- Prog/Neo4jLoader.py
+++ Prog/Neo4jLoader.py
@@ -28,21 +28,22 @@ def create_distanceGraph():
 
     # creating Organization node
     statement1 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data/orgs.csv' AS line
-        Merge (o:Organization {name: line.Organization})
+        LOAD CSV FROM 'file://%s/Data/orgs.csv' AS line with line skip 1
+        Merge (o:Organization {name: TOUPPER(line[1])})
     """
     statement1 %= currentDir
     cypher.execute(statement1)
 
-    #create relationships between oraganization
 
+
+    #create relationships between oraganization
     statement2 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data/distance.csv' AS line
-        MATCH (company1:Organization { name: line.Organization1})
-        MATCH (company2:Organization { name: line.Organization2})
+        LOAD CSV  FROM 'file://%s/Data/distance.csv' AS line
+        MATCH (company1:Organization { name: TOUPPER(line[0])})
+        MATCH (company2:Organization { name: TOUPPER(line[1])})
         MERGE (company1)-[m:DISTANCE]-(company2)
-        SET m.miles = TOINT(line.Distance),
-        m.closeby = CASE WHEN TOINT(line.Distance) < 10 THEN true ELSE false END
+        SET m.miles = TOINT(line[2]),
+        m.closeby = CASE WHEN TOINT(line[2]) < 10 THEN true ELSE false END
     """
     statement2 %= currentDir
     cypher.execute(statement2)
@@ -56,29 +57,32 @@ def create_PersonGraph():
 
     # creating person graph
     statement1 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data/names.csv' AS line
-        MERGE (p:Person { User_id: TOINT(line.User_id),
-        Fname: upper(line.FirstName), Lname: upper(line.LastName) } )
+        LOAD CSV FROM 'file://%s/Data/names.csv' AS line with line skip 1
+        MERGE (p:Person { User_id: TOINT(line[0]),
+        Fname: upper(line[1]), Lname: upper(line[2])} )
     """
     statement1 %= currentDir
     cypher.execute(statement1)
+
+
 
 
 def create_ProjectGraph():
     # creating project graph
 
     statement1 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data//projects.csv' AS line
-        MERGE(i:projects { projectname: line.Project } )
+        LOAD CSV FROM 'file://%s/Data//projects.csv' AS line with line skip 1
+        MERGE(i:projects { projectname: Tolower(line[1]) } )
     """
     statement1 %= currentDir
     cypher.execute(statement1)
 
+
     # creating relationships between person and projects
     statement2 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data//projects.csv' AS line
-        MATCH (a:Person { User_id: TOINT(line.User_id) }),
-            (b:projects { projectname:line.Project })
+        LOAD CSV FROM 'file://%s/Data//projects.csv' AS line with line skip 1
+        MATCH (a:Person { User_id: TOINT(line[0])}),
+        (b:projects { projectname: tolower(line[1]) })
         MERGE (a)-[:Working_on]->(b)
     """
 
@@ -89,9 +93,9 @@ def create_ProjectGraph():
 def create_Relationship():
     # creating relationships between person and organization
     statement1 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data//orgs.csv' AS line
-        MATCH (a:Person { User_id: TOINT(line.User_id) }),
-            (b:Organization { name:line.Organization })
+        LOAD CSV FROM 'file://%s/Data//orgs.csv' AS line with line skip 1
+        MATCH (a:Person { User_id: TOINT(line[0]) }),
+            (b:Organization { name:TOUPPER(line[1]) })
         CREATE (a)-[:Work_at]->(b)
 
     """
@@ -100,8 +104,8 @@ def create_Relationship():
 
     # creating colleague relationships
     statement2 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data//orgs.csv' AS line
-        MATCH (thisPerson{User_id: TOINT(line.User_id) })-[:Working_on]->(project)<-[:Working_on]-(Person)
+        LOAD CSV  FROM 'file://%s/Data//orgs.csv' AS line with line skip 1
+        MATCH (thisPerson{User_id: TOINT(line[0]) })-[:Working_on]->(project)<-[:Working_on]-(Person)
         WHERE NOT (thisPerson)-[:Working_on]-(Person)
         MERGE(thisPerson)-[:colleague]-(Person)
     """
@@ -112,18 +116,21 @@ def create_Relationship():
 def create_InterestGraph():
     # creating interest node
     statement1 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data/interests.csv' AS line
-        MERGE (s:interest {interestName: line.Interest})
+        LOAD CSV FROM 'file://%s/Data/interests.csv' AS line with line skip 1
+        MERGE (s:interest {interestName: tolower(line[1])})
     """
     statement1 %= currentDir
     cypher.execute(statement1)
 
     # creating relations between interest and person
     statement2 = """
-        LOAD CSV WITH HEADERS FROM 'file://%s/Data/interests.csv' AS line
-        MATCH (a:Person {User_id: TOINT(line.User_id) }), (b:interest {interestName: line.Interest})
+        LOAD CSV FROM 'file://%s/Data/interests.csv' AS line with line skip 1
+        MATCH (a:Person {User_id: TOINT(line[0]) }), (b:interest {interestName: tolower(line[1])})
         MERGE(a)-[r:interested_in]->(b)
-        SET r.level = TOINT(line.Interestlevel)
+        SET r.level = TOINT(line[2])
     """
     statement2 %= currentDir
     cypher.execute(statement2)
+
+
+load_neo4j()
