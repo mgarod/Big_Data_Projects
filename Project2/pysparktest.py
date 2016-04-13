8a9eef5dded748a61848d2373ad649983aba82fd
Michael Garod <mgarod@gmail.com>
2016-04-13 18:10:21 -0400

    Add Project2 folder and pysparktest.py

---
+++ Project2/pysparktest.py
@@ -0,0 +1,45 @@
+"""
+This test script counts the number of numbers (exclusively) divisible
+by 3, 5, and 15 from 0 to 1,000,000.
+
+You can simply copy and paste the following into the PySpark interpreter.
+"""
+
+import psutil
+
+
+def fizzbuzz(x):
+	if (x%3)==0 and (x%5)==0:
+		return 15
+	elif (x%5)==0:
+		return 5
+	elif (x%3)==0:
+		return 3
+
+
+def isInt(x):
+	try:
+		return int(x)
+	except:
+		return False
+
+
+def tuplefactory(x):
+	return (x, 1)
+
+
+def listfactory(x):
+	return (x[0], list(x[1]))
+
+
+def sum_list(x):
+	return (x[0], sum(x[1]))
+
+
+a = sc.parallelize( range(1, 1000000) )
+b = a.map(fizzbuzz)
+c = b.filter(isInt)
+d = c.map(tuplefactory)
+e = d.groupByKey()
+f = e.map(sum_list)
+f.collect()
