771124cabedd337e88ace74a17f4ddee051798c5
Michael Garod <mgarod@gmail.com>
2016-04-24 14:05:06 -0400

    Fix Total word count error

--- Project2/pysparktest.py
+++ Project2/pysparktest.py
@@ -1,5 +1,5 @@
 """
-You can run this file with root/.../bin/spark-submit
+You must run this file with /usr/local/bin/spark-submit
 """
 from pyspark import SparkConf, SparkContext
 import sys
@@ -16,7 +16,6 @@ conf = (SparkConf()
 )
 sc = SparkContext(conf = conf)
 
-
 def docid_word(x):
     l = list()
     doc_id = x[0]
@@ -26,18 +25,41 @@ def docid_word(x):
     return l
 
 # (Key, Value) ---> ( tuple (docid, word), 1) ) 
+# ex = sc.textFile("example.txt")
 ex = sc.textFile(sys.argv[1])  # Open the file, splitting on '\n'
 ex2 = ex.map(lambda x: x.split(' ', 1))  # Separate the doc id
 ex3 = ex2.map(docid_word)  # Generate (docid, word) ---> 1
 flat = ex3.flatMap(lambda x: x)  # Remove partitions separating documents
 grouped = flat.reduceByKey(lambda x,y: x+y)  # Group By Key, sum frequency
 
-print "Word Count Per Document (sorted by frequency):"
-print grouped.sortBy(lambda x: x[1], ascending=False).collect()
+print "Word Count Per Document (sorted by doc, then frequency):"
+sortbyfreq = grouped.sortBy(lambda x: x[1], ascending=False)
+sortbydoc = sortbyfreq.sortBy(lambda x: x[0][0])
+print sortbydoc.collect()
+
+###########################################################
 
 # (Key Value) ---> ( word, 1 )
-total = grouped.map(lambda x: (x[0][1], 1))  # Extract only (word, 1)
+total = grouped.map(lambda x: (x[0][1], x[1]))  # Extract only (word, freq)
 total2 = total.reduceByKey(lambda x,y: x+y)  # Traditional word count MapReduce
 
 print "\nWord Count for the Corpus (sorted by frequency):"
 print total2.sortBy(lambda x: x[1], ascending=False).collect()
+
+###########################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
