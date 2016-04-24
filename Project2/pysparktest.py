12b7c150a63757def6b33536f0c786ea209fc594
Michael Garod <mgarod@gmail.com>
2016-04-24 15:28:34 -0400

    Add term frequency ratio. Add output.txt

--- Project2/pysparktest.py
+++ Project2/pysparktest.py
@@ -15,22 +15,25 @@ conf = (SparkConf()
          #.set("spark.executor.memory", "4g")
 )
 sc = SparkContext(conf = conf)
+###########################################################
+# WORD FREQUENCY BY DOCUMENT
+# (Key, Value) ---> ( tuple (docid, word), 1) )
 
 def docid_word(x):
     l = list()
     doc_id = x[0]
     for word in x[1].split():
-        tuple = ((doc_id, word), 1)
-        l.append(tuple)
+        tup = ((doc_id, word), 1)
+        l.append(tup)
     return l
 
-# (Key, Value) ---> ( tuple (docid, word), 1) ) 
 # ex = sc.textFile("example.txt")
 ex = sc.textFile(sys.argv[1])  # Open the file, splitting on '\n'
 ex2 = ex.map(lambda x: x.split(' ', 1))  # Separate the doc id
 ex3 = ex2.map(docid_word)  # Generate (docid, word) ---> 1
 flat = ex3.flatMap(lambda x: x)  # Remove partitions separating documents
 grouped = flat.reduceByKey(lambda x,y: x+y)  # Group By Key, sum frequency
+# (Key, Value) ---> ( tuple (docid, word), docfreq) ) 
 
 print "Word Count Per Document (sorted by doc, then frequency):"
 sortbyfreq = grouped.sortBy(lambda x: x[1], ascending=False)
@@ -38,24 +41,50 @@ sortbydoc = sortbyfreq.sortBy(lambda x: x[0][0])
 print sortbydoc.collect()
 
 ###########################################################
+# TOTAL FREQUENCY IN CORPUS
+# (Key, Value) ---> ( word, freq in that document )
 
-# (Key Value) ---> ( word, 1 )
-total = grouped.map(lambda x: (x[0][1], x[1]))  # Extract only (word, freq)
-total2 = total.reduceByKey(lambda x,y: x+y)  # Traditional word count MapReduce
+total = grouped.map(lambda x: (x[0][1], x[1]))  # Extract tuple
+total2 = total.reduceByKey(lambda x,y: x+y)  # Total freq in all documents
 
 print "\nWord Count for the Corpus (sorted by frequency):"
 print total2.sortBy(lambda x: x[1], ascending=False).collect()
 
 ###########################################################
+# TERM FREQUENCY RATIO
+# We must have the total number of documents
+total_docs = int(grouped.map(lambda x: x[0][0]).max())
 
+def duplicate(x, num_docs):
+	l = list()
+	for i in range(1, num_docs+1):
+		tup = ((str(i), x[0]), x[1])
+		l.append(tup)
+	return l
 
+def get_ratio(x):
+	# The values of n1, n2 are not guaranteed to be in any order
+	n1, n2 = x[1]
+	return (x[0], float(min(n1,n2)) / float(max(n1,n2)))
 
+# Make the following for EVERY VALUE of docid
+# (Key, Value) ---> ( tuple(docid, word) , totalfreq )
+duplicatedfreq = total2.map(lambda x: duplicate(x, total_docs))
+flattotalfreq = duplicatedfreq.flatMap(lambda x: x)
 
+# Join is an inner join which collects values into tuples only where
+#  there is a match on keys between the two RDDs. Recall that 'grouped'
+#  is an RDD of (key, value) ---> ((docid, word), docfreq)
+joined = flattotalfreq.join(grouped)
 
+# Every tuple of key:(docid, word) now has a 2-tuple (docfreq, totalfreq)
+#  not necessarily in that order i.e. it could be (totalfreq, docfreq)
+termfrequency = joined.map(get_ratio)
 
-
-
-
+print "\nTerm Frequency (sorted by docid, then frequency):"
+tfbyfreq = termfrequency.sortBy(lambda x: x[1], ascending=False)
+tfbydoc = tfbyfreq.sortBy(lambda x: x[0][0])
+print tfbydoc.collect()
 
 
 
