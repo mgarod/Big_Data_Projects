df10cfbdc5ae344ecdb82404688fc7078cff267f
Michael Garod <mgarod@gmail.com>
2016-04-24 17:34:55 -0400

    Add inverse document frequency

--- Project2/pysparktest.py
+++ Project2/pysparktest.py
@@ -3,6 +3,7 @@ You must run this file with /usr/local/bin/spark-submit
 """
 from pyspark import SparkConf, SparkContext
 import sys
+import math
 
 if len(sys.argv) != 2:
         print "Usage: spark-submit pysparktest.py <file>"
@@ -15,6 +16,7 @@ conf = (SparkConf()
          #.set("spark.executor.memory", "4g")
 )
 sc = SparkContext(conf = conf)
+
 ###########################################################
 # WORD FREQUENCY BY DOCUMENT
 # (Key, Value) ---> ( tuple (docid, word), 1) )
@@ -28,10 +30,10 @@ def docid_word(x):
     return l
 
 # ex = sc.textFile("example.txt")
-ex = sc.textFile(sys.argv[1])  # Open the file, splitting on '\n'
-ex2 = ex.map(lambda x: x.split(' ', 1))  # Separate the doc id
-ex3 = ex2.map(docid_word)  # Generate (docid, word) ---> 1
-flat = ex3.flatMap(lambda x: x)  # Remove partitions separating documents
+fullfile = sc.textFile(sys.argv[1])  # Open the file, splitting on '\n'
+splitid = fullfile.map(lambda x: x.split(' ', 1))  # Separate the doc id
+id_word_tuples = splitid.map(docid_word)  # Generate (docid, word) ---> 1
+flat = id_word_tuples.flatMap(lambda x: x)  # Remove partitions separating documents
 grouped = flat.reduceByKey(lambda x,y: x+y)  # Group By Key, sum frequency
 # (Key, Value) ---> ( tuple (docid, word), docfreq) ) 
 
@@ -55,7 +57,7 @@ print total2.sortBy(lambda x: x[1], ascending=False).collect()
 # We must have the total number of documents
 total_docs = int(grouped.map(lambda x: x[0][0]).max())
 
-def duplicate(x, num_docs):
+def duplicatefreq(x, num_docs):
 	l = list()
 	for i in range(1, num_docs+1):
 		tup = ((str(i), x[0]), x[1])
@@ -69,7 +71,7 @@ def get_ratio(x):
 
 # Make the following for EVERY VALUE of docid
 # (Key, Value) ---> ( tuple(docid, word) , totalfreq )
-duplicatedfreq = total2.map(lambda x: duplicate(x, total_docs))
+duplicatedfreq = total2.map(lambda x: duplicatefreq(x, total_docs))
 flattotalfreq = duplicatedfreq.flatMap(lambda x: x)
 
 # Join is an inner join which collects values into tuples only where
@@ -81,10 +83,28 @@ joined = flattotalfreq.join(grouped)
 #  not necessarily in that order i.e. it could be (totalfreq, docfreq)
 termfrequency = joined.map(get_ratio)
 
-print "\nTerm Frequency (sorted by docid, then frequency):"
+print "\nTerm Frequency (sorted by docid, then frequency ratio):"
 tfbyfreq = termfrequency.sortBy(lambda x: x[1], ascending=False)
 tfbydoc = tfbyfreq.sortBy(lambda x: x[0][0])
 print tfbydoc.collect()
+
+###########################################################
+# INVERSE DOCUMENT FREQUENCY
+# We use total_docs from the previous calculation
+# math.log(x) returns the natural logarithm of x
+
+def idf(x):
+	return (x[0], math.log(float(num_docs) / float(x[1])))
+
+# Emit (word, 1) representing this word appears in 1 document
+# There is no duplicates b/c we are starting from WORD FREQ. BY DOC. result
+words_1 = grouped.map(lambda x: (x[0][1], 1))
+num_docs_with_word = words_1.groupByKey().map(lambda x: (x[0], sum(x[1])))
+inversedocfreq = num_docs_with_word.map(idf)
+idfbyidf = inversedocfreq.sortBy(lambda x: x[1])
+
+print "\nInverse Document Frequency (sorted by idf):"
+print idfbyidf.collect()
 
 
 
